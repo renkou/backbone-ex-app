2fa1cfff190bb0acf599ccd55ef9b9d31db895bf
renkou <slor68@gmail.com>
2016-03-01 20:02:10 -0600

    heroku testing

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -46,7 +46,6 @@ var Blog = Backbone.Model.extend({
 var Blogs = Backbone.Collection.extend({
 	//requests from this url
 	//do i need to change this for heroku
-	url: 'http://localhost:3000/api/blogs' 
 });
 
 //Test Blogs
