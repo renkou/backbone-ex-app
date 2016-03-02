286ccdaada8ad1da11145cd7467cefe7715d068c
renkou <slor68@gmail.com>
2016-03-01 18:03:06 -0600

    testing port

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.put('/api/blogs/:id', function(req, res) {
     });
 });
 
-app.set('port', process.argv[3] || process.env.PORT || 5000)
+app.set('port', process.argv[3] || process.env.PORT || 3000)
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'))
